d537cb81e46ce5bd3b0b577d7a1240494e2d39b2
1egend-Eli <harkemjefferson@gmail.com>
2022-10-19 03:58:30 +0000

    corrected task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -9,13 +9,12 @@
 int _islower(int c)
 {
 
-	for (c = 'a'; c <= 'z'; c++)
-		if (c >= 'a' && c <= 'z')
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
