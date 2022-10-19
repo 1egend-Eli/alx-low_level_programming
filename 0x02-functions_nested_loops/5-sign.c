7018301ced2944eda61e0f0258f116ea7a9d8988
1egend-Eli <harkemjefferson@gmail.com>
2022-10-19 04:49:54 +0000

    tasks 5 errors

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,14 +13,14 @@ int print_sign(int n)
 		putchar('+');
 		return (1);
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
 		putchar('-');
-		return (0);
+		return (-1);
 	}
 	else
 	{
 		putchar('0');
-		return (-1);
+		return (0);
 	}
-}	
+}
