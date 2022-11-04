b9190834de510fac235793be5a2d12cefebe3538
1egend-Eli <harkemjefferson@gmail.com>
2022-11-04 19:43:48 +0000

    task 0 fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,12 +3,13 @@
 /**
 * main - print the name of the progeam
 * @argv: argument vector
+* @argc: argument count
 * Return: always 0
 */
 
-int main(char *argv[])
+int main(int argc, char *argv[])
 {
-	/*(void) argc;*/
+	(void) argc;
 	printf("%s\n", argv[0]);
 
 	return (0);
